c957e31266f2718ab02c91e620a5618ea175a88d
mutuajames <njorog3.james@gmail.com>
2022-12-30 17:10:50 +0300

    Configured with viper

---
+++ cmd/microservice/config/config.go
@@ -0,0 +1,43 @@
+package config
+
+import (
+	"bytes"
+	"github.com/spf13/viper"
+	"strings"
+)
+
+type Postgres struct {
+	Host     string
+	User     string
+	Password string
+}
+
+type Config struct {
+	Postgres *Postgres
+}
+
+//go: embed config.yml
+var defaultConfiguration []byte
+
+func Read() (*Config, error) {
+	//Environment variables
+	viper.AutomaticEnv()
+	viper.SetEnvPrefix("APP")
+	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_", "-", "_"))
+
+	//Configuration file
+	viper.SetConfigType("yml")
+
+	//Read configuration
+	if err := viper.ReadConfig(bytes.NewBuffer(defaultConfiguration)); err != nil {
+		return nil, err
+	}
+
+	//Unmarshall the configuration
+	var config Config
+	if err := viper.Unmarshal(&config); err != nil {
+		return nil, err
+	}
+
+	return &config, nil
+}
---
+++ cmd/microservice/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"Rest-with-postgres/api/authors"
+	"Rest-with-postgres/cmd/microservice/config"
+	"Rest-with-postgres/internal/database"
+	"github.com/gin-gonic/gin"
+	"log"
+)
+
+func main() {
+	//Read the config
+	cfg, err := config.Read()
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	//Instantiates the database
+	postgres, err := database.NewPostgres(cfg.Postgres.Host, cfg.Postgres.User, cfg.Postgres.Password)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	//Instantiate author service
+	queries := database.New(postgres.DB)
+	authorService := authors.NewService(queries)
+
+	//Register our service handlers to the router
+	router := gin.Default()
+	authorService.RegisterHandlers(router)
+
+	//Start the server
+	router.Run()
+}
